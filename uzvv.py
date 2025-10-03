48fee74620b925dbdd5d16a3e202ac5be6c17e0f
ᴠͥɪͣᴘͫ✮⃝𝓜𝓪𝓺𝓼𝓪𝓭𝓫𝓮𝓴࿐✮𝄟⃝ <ekiuzb999@gmail.com>
2025-10-03 09:13:48 -0500

    20ta masala

---
+++ uzvv.py
@@ -0,0 +1,124 @@
+# #1-masala
+# a=int(input())
+# b=int(input())
+# ab2=a+b
+# print(ab2)
+
+# #2-masala
+# a=int(input())
+# b=int(input())
+# s=a*b
+# print(s)
+
+# #3-Masala
+# x=int(input())
+# y=int(input())
+# z=int(input())
+# av=(x+y+z)/3
+# print(av)
+
+# #4-masala
+# a=int(input())
+# b=int(input())
+# p=2*(a+b)
+# print(p)
+
+# #5-masala
+# soat=int(input())
+# min=int(input())
+# jami=soat*60+min
+# print(jami)
+
+# #6-masala
+# usd=int(input())
+# sum=usd*12104
+# print(sum)
+
+# #7-masala
+# a=int(input())
+# b=int(input())
+# dif=abs(a-b)
+# print(dif)
+
+# #8-masala
+# x=int(input())
+# n=int(input())
+# pov=x**n
+# print(pov)
+
+# #9-masala
+# a=int(input())
+# b=int(input())
+# quo=a//b
+# print(quo)
+
+# #10-masala
+# a=int(input())
+# b=int(input())
+# a,b=b,a
+# print(a,b)
+
+# #11-masala
+# total=int(input())
+# n=int(input())
+# per=total/n
+# print(per)
+
+# #12-masala
+# d=int(input())
+# v=int(input())
+# time=d/v
+# print(time)
+
+# #13-masala
+# seat=int(input())
+# passen=int(input())
+# perperson=passen//seat
+# left=passen%seat
+# print(left)
+
+# #14-masala
+# con=int(input())
+# d=int(input())
+# fue=con*d/100
+# print(fue)
+
+# #15-masala
+# salary=int(input())
+# net=salary * 0.88
+# print(net)
+
+# #16-masala
+# amount=float(input())
+# days=int(input())
+# penalty=amount*0.001*days
+# tot=amount+penalty
+# print(tot)
+
+# #17-masala
+# price=int(input())
+# discount=int(input())
+# paid=price*(1-dicount/100)
+# print(paid)
+
+# #18-masala
+# P=float(input())
+# r=float(input())
+# t=int(input())
+# inst=(P*r*t)/100
+# total=P+inst
+# print(inst)
+# print(total)
+
+# #19-masala
+# base=int(input())
+# peop=int(input())
+# perp=base/4
+# ned=perb*peop
+# print(ned)
+
+# #20-masala
+# bill=int(input())
+# tip=bill*0.10
+# total=bill+tip
+# print(total)
